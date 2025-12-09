7427a5d50077a9a15f2b9423f9e96fcbcad50949
Rawan-Khalifa <rawan@uni.minerva.edu>
2025-12-09 13:56:06 +0800

    feat: Prometheus metrics for Raft consensus monitoring

---
+++ metrics.py
@@ -0,0 +1,139 @@
+"""
+Prometheus metrics for Raft consensus monitoring
+"""
+import time
+from typing import Dict
+from enum import Enum
+
+class MetricType(Enum):
+    COUNTER = "counter"
+    GAUGE = "gauge"
+    HISTOGRAM = "histogram"
+
+class Metrics:
+    """Prometheus metrics collector"""
+    
+    def __init__(self):
+        # Counter metrics
+        self.elections_total = 0
+        self.leader_elections_total = 0
+        self.lost_elections_total = 0
+        
+        # Gauge metrics (instantaneous values)
+        self.current_term = 0
+        self.commit_index = 0
+        self.last_applied = 0
+        self.log_size = 0
+        self.follower_count = 0
+        self.peer_count = 0
+        
+        # Histogram metrics
+        self.election_latency_ms = []
+        self.replication_latency_ms = []
+        
+        # State tracking
+        self.last_election_time = None
+        self.started_at = time.time()
+        self.leader_changes = 0
+        self.last_leader_id = None
+    
+    def record_election_start(self):
+        """Record start of election"""
+        self.last_election_time = time.time()
+        self.elections_total += 1
+    
+    def record_election_won(self):
+        """Record successful election"""
+        self.leader_elections_total += 1
+        if self.last_election_time:
+            latency = (time.time() - self.last_election_time) * 1000
+            self.election_latency_ms.append(latency)
+    
+    def record_election_lost(self):
+        """Record failed election"""
+        self.lost_elections_total += 1
+    
+    def record_leader_change(self, new_leader_id: str):
+        """Record leader change"""
+        if self.last_leader_id != new_leader_id:
+            self.leader_changes += 1
+            self.last_leader_id = new_leader_id
+    
+    def record_replication_latency(self, latency_ms: float):
+        """Record replication latency"""
+        self.replication_latency_ms.append(latency_ms)
+    
+    def update_state(self, current_term: int, commit_index: int, 
+                     last_applied: int, log_size: int, peer_count: int):
+        """Update gauge metrics"""
+        self.current_term = current_term
+        self.commit_index = commit_index
+        self.last_applied = last_applied
+        self.log_size = log_size
+        self.peer_count = peer_count
+    
+    def get_metrics(self) -> Dict:
+        """Get all metrics as dictionary"""
+        uptime_seconds = time.time() - self.started_at
+        
+        # Calculate averages
+        avg_election_latency = (
+            sum(self.election_latency_ms) / len(self.election_latency_ms)
+            if self.election_latency_ms else 0
+        )
+        
+        avg_replication_latency = (
+            sum(self.replication_latency_ms) / len(self.replication_latency_ms)
+            if self.replication_latency_ms else 0
+        )
+        
+        return {
+            'uptime_seconds': uptime_seconds,
+            'elections_total': self.elections_total,
+            'leader_elections_total': self.leader_elections_total,
+            'lost_elections_total': self.lost_elections_total,
+            'leader_changes_total': self.leader_changes,
+            'current_term': self.current_term,
+            'commit_index': self.commit_index,
+            'last_applied': self.last_applied,
+            'log_size': self.log_size,
+            'peer_count': self.peer_count,
+            'avg_election_latency_ms': avg_election_latency,
+            'avg_replication_latency_ms': avg_replication_latency,
+            'election_latency_samples': len(self.election_latency_ms),
+            'replication_latency_samples': len(self.replication_latency_ms),
+        }
+    
+    def prometheus_format(self, node_id: str) -> str:
+        """Export metrics in Prometheus text format"""
+        metrics = self.get_metrics()
+        lines = [
+            f"# HELP raft_elections_total Total number of elections",
+            f"# TYPE raft_elections_total counter",
+            f'raft_elections_total{{node="{node_id}"}} {metrics["elections_total"]}',
+            f"",
+            f"# HELP raft_leader_elections_total Total successful leader elections",
+            f"# TYPE raft_leader_elections_total counter",
+            f'raft_leader_elections_total{{node="{node_id}"}} {metrics["leader_elections_total"]}',
+            f"",
+            f"# HELP raft_lost_elections_total Total failed elections",
+            f"# TYPE raft_lost_elections_total counter",
+            f'raft_lost_elections_total{{node="{node_id}"}} {metrics["lost_elections_total"]}',
+            f"",
+            f"# HELP raft_current_term Current Raft term",
+            f"# TYPE raft_current_term gauge",
+            f'raft_current_term{{node="{node_id}"}} {metrics["current_term"]}',
+            f"",
+            f"# HELP raft_commit_index Highest committed log index",
+            f"# TYPE raft_commit_index gauge",
+            f'raft_commit_index{{node="{node_id}"}} {metrics["commit_index"]}',
+            f"",
+            f"# HELP raft_log_size Number of entries in log",
+            f"# TYPE raft_log_size gauge",
+            f'raft_log_size{{node="{node_id}"}} {metrics["log_size"]}',
+            f"",
+            f"# HELP raft_uptime_seconds Node uptime",
+            f"# TYPE raft_uptime_seconds gauge",
+            f'raft_uptime_seconds{{node="{node_id}"}} {metrics["uptime_seconds"]:.2f}',
+        ]
+        return "\n".join(lines)
